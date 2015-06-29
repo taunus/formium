b8961d031c60196fb3ff5a6ed087261236d8cf84
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2015-06-28 23:30:02 -0300

    initial commit

---
+++ formium.js
@@ -0,0 +1,114 @@
+'use strict';
+
+// jshint scripturl:true
+
+var $ = require('dominus');
+var queso = require('queso');
+var safeson = require('safeson');
+var state = require('./lib/state');
+var transformers = [];
+var formium = {
+  submit: submit,
+  transform: transform,
+  configure: state.configure
+};
+
+function noop () {}
+
+function transform (fn) {
+  transformers.push(fn);
+}
+
+/* AJAX form submissions use an intermediary <iframe> to help browsers remember
+ * autocompletion suggestions. We submit the form against the <iframe> and then
+ * grab the response as plain-text JSON that we need to parse into JSON.
+ * Afterwards we can hand that JSON to the response handler, business as usual.
+ * Since we've submitted the form "organically", the browser stores suggestions.
+ */
+function frame (form) {
+  var name = 'ff-' + new Date().valueOf();
+  $(form)
+    .attr('autocomplete', 'on')
+    .attr('target', name);
+  return $('<iframe>')
+    .css('display', 'none')
+    .attr('id', name)
+    .attr('name', name)
+    .attr('src', 'javascript:void 0')
+    .afterOf(form);
+}
+
+function submit (form, done) {
+  var textareaCloneValue = 'data-clone-value';
+  var iframe = frame(form);
+  var content = iframe[0].contentWindow;
+  var restore = transformers.map(run);
+  var textareas = $('textarea', form);
+  textareas.forEach(preserveValue);
+  var formClone = form.cloneNode(true);
+  var textareaClones = $('textarea', formClone);
+  disable(form);
+  $('button', form).forEach(disable);
+  $('[autofocus]', formClone).attr('autofocus', null);
+  textareaClones.forEach(updateValue);
+  textareas.forEach(restoreTextarea);
+  var formCloneId = 'f' + iframe[0].id;
+  formClone.id = formCloneId;
+  iframe.once('load', grabResponse);
+  content.document.body.appendChild(formClone);
+  var frameForm = content.document.getElementById(formCloneId);
+  var amp = frameForm.action.indexOf('?') !== -1;
+  frameForm.action += queso.stringify(state.qs(form), amp);
+  frameForm.onsubmit = null;
+  frameForm.submit();
+  restore.forEach(run);
+  function preserveValue (textarea) {
+    var ta = $(textarea);
+    ta.attr(textareaCloneValue, ta.value());
+  }
+  function updateValue (textarea) {
+    var ta = $(textarea);
+    ta.value(ta.attr(textareaCloneValue)).attr(textareaCloneValue, null);
+  }
+  function restoreTextarea (textarea) {
+    $(textarea).attr(textareaCloneValue, null);
+  }
+  function grabResponse () {
+    var html = readResponse(content);
+    if (!html) {
+      gotResponse(new Error('Internal Server Error')); return;
+    }
+    var json = decodeResponse(html);
+    var err = json ? null : new Error('Malformed response');
+    gotResponse(err, json);
+    function gotResponse (err, data) {
+      $('button', form).forEach(enable);
+      enable(form);
+      iframe.remove();
+      (done || noop).call(form, err, data);
+    }
+  }
+  function readResponse (content) {
+    try {
+      return content.document.body.innerHTML;
+    } catch (e) { // failure to read (most likely) means server crashed or response timed out
+    }
+  }
+  function decodeResponse (html) {
+    try {
+      return safeson.decode(html);
+    } catch (e) {
+    }
+  }
+  function run (fn) {
+    return (fn || noop)(form);
+  }
+  function disable (el) {
+    el.disabled = true;
+  }
+  function enable (el) {
+    el.disabled = false;
+  }
+}
+
+module.exports = formium;
---
+++ readme.markdown
@@ -0,0 +1,80 @@
+# formium
+
+> Automated progressive enhancement for `<form>` elements
+
+# Install
+
+```shell
+npm install formium --save
+```
+
+# Usage
+
+Formium is a progressive enhancement facility that allows you to seamlessly submit plain old HTML forms via AJAX and handle the response in a conventional manner.
+
+### `formium.submit(form, done?)`
+
+Exactly as you would expect, this method will submit a `<form>` asynchronously via AJAX.
+
+```js
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  formium.submit(form, function handler (err, data) {
+    // handle the response
+  }
+});
+```
+
+The form submission mechanism provided by `formium` will:
+
+- Run any [transformers](#formiumtransformfn) on the `<form>` elements before submission
+- Submit the `<form>` from within an `<iframe>`
+  - The form submission is effectively AJAX
+  - Browser loading indicator while the form is being posted _(unlike AJAX)_
+  - Form auto-fill values are persisted by the browser _(unlike AJAX)_
+- Run the restoration callback for each one of the previously applied [transformers](#formiumtransformfn)
+- Disable the `<form>` and `<button>`s inside it while the form loads
+- Grab the response once the `<iframe>` finishes loading
+- Call `done(err, data)` with `this` set to the `form`
+
+For maximum usefulness, we recommend that you come up with some conventions to handle forms submitted via `formium`. For example, you could make it so responses like `{ redirect: '/foo' }` end up redirecting the user to `/foo` (or rendering that view with a client-side view rendering engine). That convention alone should simplify your application so that you don't have to treat individual forms differently from one another.
+
+### `formium.configure(options)`
+
+You can set a `qs` option that will be used to construct a query string appended to each AJAX form request.
+
+```js
+formium.configure({
+  qs: function (form) {
+    return {
+      foo: 'bar'
+    };
+  }
+});
+```
+
+### `formium.transform(fn)`
+
+This method allows you to register a `fn(form)` callback that gets called whenever a `form` is submitted. You can prepare the form for submission in any way you want. You can optionally return a callback that restores the form to its original state.
+
+The use case for Formium transforms is for those cases when you have a UX enhancement that breaks the state of the form, say if you were using [insignia][1]. In those cases, you can turn the field's value into something plain, and restore the more complex [insignia][1] tag editor right afterwards.
+
+```js
+formium.transform(function fix (form) {
+  $(form).find('.nsg-input').forEach(function (input) {
+    input.attr('data-prev', input.value());
+    input.value(insignia(input).value());
+  });
+  return function restore () {
+    $(form).find('.nsg-input').forEach(function (input) {
+      input.value(input.attr('data-prev'));
+    });
+  };
+});
+```
+
+# License
+
+MIT
+
+[1]: https://github.com/bevacqua/insignia
